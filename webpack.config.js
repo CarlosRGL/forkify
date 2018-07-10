c17a8d6fea3caa0b4006bdb8894350fd5c4ddc26
CarlosRGL <carlosruizgl@gmail.com>
2018-07-10 16:38:11 +0200

    added babel and config

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,29 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: ['babel-polyfill', './src/js/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/bundle.js'
   },
   devServer: {
     contentBase: './dist'
-<<<<<<< HEAD
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './src/index.html'
     })
-  ]
-=======
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
   }
->>>>>>> 64e63df47144e17f4a4d34434def6154f6c6666d
 };
