3c6b517cfd490b0d652f32931ae3ec73a47b36f0
CarlosRGL <carlosruizgl@gmail.com>
2018-07-13 16:10:58 +0200

    added webpack dashboard to the dev process

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const DashboardPlugin = require('webpack-dashboard/plugin');
 
 module.exports = {
   entry: ['babel-polyfill', './src/js/index.js'],
@@ -14,7 +15,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './src/index.html'
-    })
+    }),
+
+    new DashboardPlugin()
   ],
   module: {
     rules: [
