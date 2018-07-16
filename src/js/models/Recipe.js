5e3ad719251dd39cda7cde9f395be72f73f97292
CarlosRGL <carlosruizgl@gmail.com>
2018-07-16 11:06:43 +0200

    build recipe model

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -13,11 +13,22 @@ export default class Recipe {
       );
       this.title = res.data.recipe.title;
       this.author = res.data.recipe.publisher;
-      this.img = res.data.recipe.img_url;
+      this.img = res.data.recipe.image_url;
       this.url = res.data.recipe.source_url;
       this.ingredients = res.data.recipe.ingredients;
     } catch (error) {
       alert(error);
     }
+  }
+
+  calcTime() {
+    // We need 15 min for every 3 ingredients
+    const numIng = this.ingredients.length;
+    const periods = Math.ceil(numIng / 3);
+    this.time = periods * 15;
+  }
+
+  calcServings() {
+    this.servings = 4;
   }
 }
