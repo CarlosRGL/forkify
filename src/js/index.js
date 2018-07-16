81349a65966f3cfbabb9637a79579008fad91a4b
CarlosRGL <carlosruizgl@gmail.com>
2018-07-16 15:34:12 +0200

    recipe model

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
@@ -67,6 +68,7 @@ const controlRecipe = async () => {
 
   if (id) {
     // Prepare the UI for changes
+    renderLoader(elements.recipe);
 
     // Create new recipe object
     state.recipe = new Recipe(id);
@@ -82,7 +84,8 @@ const controlRecipe = async () => {
       state.recipe.calcTime();
       state.recipe.calcServings();
       // Render the recipe
-      console.log(state.recipe);
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
     } catch (error) {
       alert(error);
     }
