76d3f6a7e4541f18c88209da35d4ee1b76912c20
CarlosRGL <carlosruizgl@gmail.com>
2018-07-17 14:33:57 +0200

    📦 NEW: update ingredient when update servings

--- src/js/index.js
+++ src/js/index.js
@@ -100,3 +100,21 @@ const controlRecipe = async () => {
 ['hashchange', 'load'].forEach(event =>
   window.addEventListener(event, controlRecipe)
 );
+
+// handling recipe button click
+elements.recipe.addEventListener('click', e => {
+  if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+    // Decrease button is clicked
+    if (state.recipe.servings > 1) {
+      state.recipe.updateServings('dec');
+      recipeView.updateServingsIngredients(state.recipe);
+    }
+  }
+  if (e.target.matches('.btn-increase, .btn-increase *')) {
+    // increase button is clicked
+    state.recipe.updateServings('inc');
+    recipeView.updateServingsIngredients(state.recipe);
+  }
+
+  console.log(state.recipe);
+});
